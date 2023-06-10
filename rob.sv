91cdc902f6c4d821cf9740a7d7f98cd5db450ef4
Hayden Beames <90889247+haydenbeames@users.noreply.github.com>
2023-06-10 15:42:33 -0600

    Update rob.sv

--- rob.sv
+++ rob.sv
@@ -4,27 +4,29 @@
 
 //include files
 `include "rtl_constants.sv"
+`include "structs.sv"
 
-module rob_hb(
+module rob(
     input wire logic clk, rst,
     
     //inputs from id stage
     input wire logic [ISSUE_WIDTH_MAX-1:0]               instr_val_id,
-    input wire logic [ISSUE_WIDTH_MAX-1:0][DATA_LEN-1:0]        pc_id,
     input wire logic [ISSUE_WIDTH_MAX-1:0][SRC_LEN-1:0]         rd_id,
-    input wire logic [ISSUE_WIDTH_MAX-1:0][OPCODE_LEN-1:0]      op_id,
+    input instr_info_t [ISSUE_WIDTH_MAX-1:0] instr_info_id,
     
     //cdb inputs for commit
     input wire logic [CDB_NUM_LANES-1:0][ROB_SIZE_CLOG-1:0]  robid_cdb,
     input wire logic [CDB_NUM_LANES-1:0][31:0]              result_cdb,
     input wire logic [CDB_NUM_LANES-1:0]                       val_cdb,
     
+    
     //outputs for retire to r-rat & prf
     output logic [ROB_MAX_RETIRE-1:0][DATA_LEN-1:0]  data_ret,
     output logic [ROB_MAX_RETIRE-1:0][SRC_LEN-1:0]     rd_ret,
     output logic [ROB_MAX_RETIRE-1:0][DATA_LEN-1:0]    pc_ret,
     output logic [ROB_MAX_RETIRE-1:0]                 val_ret,
-    output logic [ROB_MAX_RETIRE-1:0]              branch_ret,  //fill in later    
+    output logic [ROB_MAX_RETIRE-1:0]             rfWrite_ret,
+    output logic [ROB_MAX_RETIRE-1:0]            memWrite_ret,  //maybe put info in a struct
     output logic [ROB_MAX_RETIRE-1:0][ROB_SIZE_CLOG-1:0] robid_ret,
     output logic [ROB_SIZE_CLOG-1:0] ret_ptr, //to R-RAT
     
@@ -39,30 +41,16 @@ module rob_hb(
     logic [ROB_MAX_RETIRE_CLOG:0] num_instr_2_ret;
     logic                         num_ret_qual;
     
-    typedef struct packed { 
-        logic [6:0]  op;
-        logic [4:0]  rd;
-        logic [31:0] pc;
-        logic [31:0] data; //branch should just use data to take of pc
-        logic        v;
-        logic        done;
-        logic        memToReg;
-        
-    } rob_t;
     rob_t [ROB_SIZE-1:0] rob;
     
     initial begin
         for (int i = 0; i < ROB_SIZE; i++) begin
-            rob[i].op   = '0;
-            rob[i].rd   = '0;
-            rob[i].pc   = '0;
-            rob[i].data = '0;
-            rob[i].v    =  0;
-            rob[i].done =  0; 
+            rob[i] = '{default:0};
             is_ptr = '0;
         end
     end
     
+    /*
     // new instr to rob
     always_ff@(posedge clk) begin
         case (instr_val_id & ~{rob_full,rob_full}) // & ~rob_full
@@ -102,7 +90,21 @@ module rob_hb(
             default: begin end
         endcase
     end
+    */
     
+    //new rob issue with varied issue width  
+    always_ff@(posedge clk) begin 
+        for (int i = 0; i < ISSUE_WIDTH_MAX; i++) begin 
+            if (instr_val_id[i]) begin
+                rob[is_ptr[i]].rd   <= rd_id[i];
+                rob[is_ptr[i]].pc   <= instr_info_id[i].pc;
+                rob[is_ptr[i]].v    <= 1;
+                rob[is_ptr[i]].done <= 0;
+                rob[is_ptr[i]].rfWrite  <= instr_info_id[i].ctrl_sig.rfWrite;
+                rob[is_ptr[i]].memWrite <= instr_info_id[i].ctrl_sig.memWrite;
+            end
+        end
+    end
     //assign is_ptr[1] = is_ptr + 1;
 
     always_comb begin
@@ -128,12 +130,12 @@ module rob_hb(
             if ((num_instr_2_ret >= (i+1)) && ((ret_ptr+i) <= ROB_SIZE)) begin
                 data_ret[i] <= rob[ret_ptr+i].data;
                 rd_ret[i]   <= rob[ret_ptr+i].rd;
-                pc_ret[i]   <= branch_ret[i] ? rob[ret_ptr+i].pc : '0;
+                pc_ret[i]   <= rob[ret_ptr+i].pc;
                 val_ret[i]  <= 1;
                 
                 rob[ret_ptr+i].v    <= 0;
                 rob[ret_ptr+i].done <= 0;
-                //add in branch_ret
+
             end else begin
                 val_ret[i]  <= 0;
             end
