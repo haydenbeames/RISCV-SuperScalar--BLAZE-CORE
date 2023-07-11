23d1003f7885bb4210247985c560d4498e5802f6
Hayden Beames <90889247+haydenbeames@users.noreply.github.com>
2023-07-10 19:48:49 -0700

    Update blaze_core_top.sv

--- blaze_core_top.sv
+++ blaze_core_top.sv
@@ -26,10 +26,12 @@
 module blaze_core_top(
     input wire logic clk, rst,
     input wire logic [15:0] instr_id_input,
-    
+       
     output [ROB_SIZE_CLOG-1:0] robid_out
+    
     );
     
+    cdb_in [CPU_NUM_LANES-1:0] cdb_in_t;
     
     //////////////////////////////////////////////////////////////////
     ///
@@ -58,7 +60,13 @@ module blaze_core_top(
     logic [ISSUE_WIDTH_MAX-1:0]      		instr_val_ar;
 
     instr_info_t [ISSUE_WIDTH_MAX-1:0]     instr_info_ar; //important instr. info passed down pipeline
+    //inputs from functional units
+	logic [CPU_NUM_LANES-1:0] fu_free, fu_free_1c; //fu_free_1c means fu free in 1 cycle
     
+	//rs OUTPUTS TO EXECUTION UNITS
+	int_alu_lane_t [NUM_INT_ALU_LN+INT_ALU_LN_OFFSET-1:INT_ALU_LN_OFFSET] int_alu_ln_info_ex1;
+	int_mul_lane_t [NUM_INT_MUL_LN+INT_MUL_LN_OFFSET-1:INT_MUL_LN_OFFSET] int_mul_ln_info_ex1;
+	logic rs_full;
     //*****strictly testing signals*****
     logic [ISSUE_WIDTH_MAX-1:0][OPCODE_LEN-1:0] opcode_id;
     logic [ISSUE_WIDTH_MAX-1:0][FUNC3_WIDTH-1:0] func3_id;
@@ -110,15 +118,68 @@ module blaze_core_top(
 	//inputs from functional units
 	logic [CPU_NUM_LANES-1:0] fu_free, fu_free_1c; //fu_free_1c means fu free in 1 cycle
     
-	//rs OUTPUTS TO EXECUTION UNITS
-	alu_lane_t [NUM_ALU_LANES-1:0] alu_lane_info_ex1;
-	logic 	   rs_full;
-	
 	assign robid_out = alu_lane_info_ex1[0].robid;
     
     //rs rs_t(.*);
       
     // END FRAT INSTANCE
     /////////////////////////////////////////////////////////////////////
+    
+    
+    ////////////////////////////////////
+    // Integer Multipliers 
+
+    genvar g_i, g_ln;
+    
+    generate
+        for (g_i = 0; g_i < NUM_INT_MUL_LN; g_i++) begin
+            int_mul int_mul_t(.clk(clk),
+                              .mul_val(int_mul_ln_info_ex1[g_i].val),
+                              .op1(int_mul_ln_info_ex1[g_i].src[RS_1]),
+                              .op2(int_mul_ln_info_ex1[g_i].src[RS_2]),
+                              .func3(int_mul_ln_info_ex1[g_i].func3),
+                              .robid(int_mul_ln_info_ex1[g_i].robid),
+                              .mul_val(   cdb_in[g_i+INT_MUL_LN_OFFSET].v),
+                              .robid_ex1( cdb_in[g_i+INT_MUL_LN_OFFSET].robid),
+                              .mul_result(cdb_in[g_i+INT_MUL_LN_OFFSET].data)
+                              );                              
+        end
+    endgenerate
+    
+    
+    //////////////////////////////////////////
+    // Integer ALUs  
+    
+    generate
+        for (g_ln = INT_ALU_LN_OFFSET; g_ln < INT_ALU_LN_OFFSET+NUM_INT_ALU_LN; g_ln++) begin
+            int_alu in_alu_t(.alu_val_ex1(int_alu_ln_info_ex1[g_ln].v),
+                             .op1_ex1(int_alu_ln_info_ex1[g_ln].src[RS_1]),
+                             .op2_ex1(int_alu_ln_info_ex1[g_ln].src[RS_2]),
+                             .alu_ctrl_ex1(int_alu_ln_info_ex1[g_ln].alu_ctrl),
+                             .robid_ex1(int_alu_ln_info_ex1[g_ln].robid),
+                             .alu_val(cdb_in[g_ln].v),
+                             .robid(cdb_in[g_ln].robid),
+                             .result_alu(cdb_in[g_ln].data)
+                             );
+        end      
+    endgenerate
+    
+    ///////////////////////////////////
+    // Common Data Bus (CDB)
+    
+    generate
+        for (g_ln = 0; g_ln < CPU_NUM_LANES; g_ln++) begin
+            cdb cdb_t(.v(cdb_in[g_ln].v),
+                      .robid(cdb_in[g_ln].robid),
+                      .data(cdb_in[g_ln].data)
+                      );
+        end
+    endgenerate
+    
+    ///////////////////////////////////
+    // Re-Order Buffer (ROB)
+    
+    rob rob_t(.*);
+    
     
 endmodule
