67285b3a98b44e493f4930c7137318bcf4f430a0
Hayden Beames <90889247+haydenbeames@users.noreply.github.com>
2023-08-04 21:11:16 -0700

    Create beu.sv

---
+++ beu.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/04/2023 09:03:53 PM
+// Design Name: 
+// Module Name: beu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//Branch Execution Unit
+module beu(
+    input wire logic clk,
+    );
+endmodule
